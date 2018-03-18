b2c68b832c0d1a64d2f7798a5c07ba67d2b035ad
David Langford <langfod@gmail.com>
2018-03-18 16:58:26 -0400

    adding comments via POST rest hook

--- src/main/java/com/dihelix/langfod/reviewssitefullstack/ReviewsController.java
+++ src/main/java/com/dihelix/langfod/reviewssitefullstack/ReviewsController.java
@@ -1,5 +1,6 @@
 package com.dihelix.langfod.reviewssitefullstack;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,7 +76,16 @@ public class ReviewsController {
 	public Iterable<Tag> apiTags() {
 		return tagRepository.findAll();
 	}
-
+	
+	@ResponseBody
+	@PostMapping("/api/review/{id}/comment")
+    public Comment greetingSubmit(@PathVariable("id") Long reviewId, @ModelAttribute Comment comment) {
+		comment.setCommentDate(LocalDateTime.now());
+		comment.setReview(reviewRepository.findById(reviewId).get());
+		comment = commentRepository.save(comment);
+        return comment;
+    }
+	
 	@ResponseBody
 	@PutMapping(value = "/api/review/{id}/tag", consumes = "application/json")
 	public ResponseEntity<String> apiReviewAddTags(@PathVariable("id") Long inputId,
