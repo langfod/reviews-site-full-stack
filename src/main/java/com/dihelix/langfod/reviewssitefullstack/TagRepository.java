73a1fac5732aa731f34fd5b793689077fd4da180
David Langford <langfod@gmail.com>
2018-03-19 06:49:05 -0400

    reworked the populator from Brian's advice

--- src/main/java/com/dihelix/langfod/reviewssitefullstack/TagRepository.java
+++ src/main/java/com/dihelix/langfod/reviewssitefullstack/TagRepository.java
@@ -1,11 +1,14 @@
 package com.dihelix.langfod.reviewssitefullstack;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface TagRepository extends CrudRepository<Tag, Long> {
 	
 	public Optional<Tag> findByName(String name);
+
+	public Set<Tag> findAllByNameIn(String[] tagNames);
 
 }
